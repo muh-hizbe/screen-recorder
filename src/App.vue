8811d53b950fcdfacbaa047660e9d0e067a879a0
Muhammad Hizbullah <muh-hizbe@users.noreply.github.com>
2021-02-13 15:04:09 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="min-h-screen">
+        <div class="animate-bounce border-t-4 border-blue-500 mb-5"></div>
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-semibold text-center underline leading-loose"><span class="bg-white border-2 border-gray-700 p-3 rounded">🖥 Screen Media</span></h1>
+            <p class="text-center font-semibold leading-loose">Screen recording</p>
+            
+            <div class="block md:hidden text-center leading-loose mt-10 text-2xl px-5 underline">Tidak mendukung di browser mobile</div>
+            
+            <div class="text-center mt-10" :class="stop ? 'grid grid-cols-2 gap-2 mx-96' : ''">
+                <button v-if="stop" @click="startRecording" class="start transition duration-300 ease-in-out font-semibold border-2 border-blue-500 px-3 py-2 rounded hover:bg-blue-500 hover:text-white">⏺ Start Recording</button>
+                <button v-if="start" @click="stopRecording" class="stop transition duration-300 ease-in-out font-semibold border-2 border-red-500 px-3 py-2 rounded hover:bg-red-500 hover:text-white">⏹ Stop Recording</button>
+                <button v-if="stop" @click="() => { videoSrc = null }" class="stop transition duration-300 ease-in-out font-semibold border-2 border-red-500 px-3 py-2 rounded hover:bg-red-500 hover:text-white">🔴 Reset</button>
+            </div>
+            <div v-if="videoSrc" class="transition duration-300 text-center py-5">
+                <a :href="videoSrc" download class="transition duration-300 ease-in-out animate-bounce font-semibold border-2 border-green-500 px-3 py-2 rounded hover:bg-green-500 hover:text-white">💾 Unduh Video</a>
+            </div>
+            
+            <div v-if="start" class="text-center text-red-700 mb-5 leading-loose underline">Don't close or refresh this page while recording !</div>
+            
+            <div v-if="start" class="flex text-center justify-center">
+                <div class="animate-bounce">🟢</div>
+                <div class="animate-pulse"> Recording ...</div>
+            </div>
+
+            <Video v-if="videoSrc" :src="videoSrc" class="transition duration-300 my-5 text-center" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Video from './components/Video.vue'
+export default {
+    name: 'App',
+
+    components: {
+        Video
+    },
+
+    created() {
+        if (!navigator.getDisplayMedia && !navigator.mediaDevices.getDisplayMedia) {
+            const msg = "Browser tidak mendukung getDisplayMeida API"
+            throw new Error(msg)
+        }
+    },
+
+    data() {
+        return {
+            start: false,
+            stop: true,
+            stream: null,
+            recorder: null,
+            videoSrc: null
+        }
+    },
+
+    methods: {
+        async startRecording() {
+            this.start = true
+            this.stop = false
+            console.log('starting recording');
+            this.stream = await navigator.mediaDevices.getDisplayMedia({
+                video: { mediaSource: 'screen'}
+            })
+            this.recorder = new MediaRecorder(this.stream)
+
+            const chunks = []
+            this.recorder.ondataavailable = (event) => chunks.push(event.data)
+            this.recorder.onstop = e => {
+                const completeBlob = new Blob(chunks, { type: chunks[0].type });
+                this.videoSrc = URL.createObjectURL(completeBlob);
+            };
+            this.recorder.start();
+        },
+        stopRecording() {
+            this.start = false
+            this.stop = true
+
+            this.recorder.stop()
+            this.stream.getVideTracks()[0].stop()
+            console.log('stop recording');
+            
+        }
+    }
+}
+</script>
---
+++ src/components/Video.vue
@@ -0,0 +1,19 @@
+<template>
+    <video id="player" playsinline controls data-plyr-config='{ "hideControls": false, "resetOnEnd": true }'
+        class="border-2 border-black rounded">
+        <source :src="src" type="video/mp4" />
+        <!-- <source :src="src" type="video/webm" /> -->
+
+        <!-- Captions are optional -->
+        <track kind="captions" label="English captions" src="/path/to/captions.vtt" srclang="id" default />
+    </video>
+    <!-- <video autoplay controls :src="src" /> -->
+</template>
+
+<script>
+import Plyr from 'plyr';
+const player = new Plyr('#player');
+export default {
+    props: ['src']
+}
+</script>
